8d92c901f52254fa8d30c707f2852fd1b9ed6e7b
Kunal Nathani <kunal.n@somaiya.edu>
2025-07-27 17:37:12 +0530

    Initial commit

--- counter/src/app/app.ts
+++ counter/src/app/app.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
